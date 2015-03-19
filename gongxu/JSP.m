74a4d461105b946842e0a60484b7df60f3141476
Zhenjiang Wang <wangzj_0826@163.com>
2015-03-19 18:14:46 +0800

    fix some bugs

--- gongxu/JSP.m
+++ gongxu/JSP.m
@@ -1,7 +1,7 @@
 %% [MinVal,P] = JSP(T,Jm,NIND,MAXGEN,GGAP,XOVR,MUTR)
 % 参数说明：
-% T          - 各工件各工序使用的时间 
-% Jm         - 各工件各工序使用的机器 
+% T          - 各工件各工序使用的时间
+% Jm         - 各工件各工序使用的机器
 % NIND       - 个体数目(Number of individuals)
 % MAXGEN     - 最大遗传代数(Maximum number of generations)
 % GGAP       - 代沟(Generation gap)
@@ -19,16 +19,16 @@ function [MinVal,P] = JSP(T,Jm,NIND,MAXGEN,GGAP,XOVR,MUTR)
 
 
 
-[PNumber,MNumber]=size(T);              % PNumber工件数量   %MNumber单个工件工序的数量
+[PNumber,MNumber]=size(T);              % PNumber工件数量   % MNumber单个工件工序的数量
 gen=0;                                  % 代计数器
 trace=zeros(2, MAXGEN);                 % 寻优结果的初始值
 
-% 初始化群            
+% 初始化群
 % 基于调度优先级的编码方法：每个基因对应一道工序，代表该工序在进行调度操作时的处理优先级。
 % 例如一个3!3问题，则染色体用1-3的整数表示为： [1 3 2 1 1 3 3 2 2]
 % 基因与工序的对应关系如下：
-%  P11  P31  P21  P12  P13  P32 P33  P22 P23
-%  [1   3    2   1   1   3   3  2  2   ]
+%  P11  P31  P21  P12  P13  P32  P33  P22  P23
+%  [1    3    2    1    1    3    3    2    2 ]
 % 其中Pij表示第i个零件的第j道工序
 WNumber=PNumber*MNumber;                % 工序总数量
 Chrom=zeros(NIND,WNumber);              % 个体集合矩阵
@@ -37,10 +37,11 @@ for i=1:PNumber
     Number(i)=MNumber;
 end
 
+% 随机生成工序序列
 for j=1:NIND
     WPNumberTemp=Number;
     for i=1:WNumber
-        val=unidrnd(PNumber);
+        val=unidrnd(PNumber);          % unidrnd: Discrete uniform random numbers，one to N
         
         while WPNumberTemp(val)==0     % 判断该工件是否编码完
             val=unidrnd(PNumber);
@@ -53,6 +54,9 @@ end
 
 % 计算目标函数值
 [PVal, ObjV, P]=cal(Chrom,T,Jm);
+% PVal      - 为最佳调度工序时间
+% ObjV      - 为群中每个个体的调度工序时间
+% P         - 为最佳输出的调度工序
 
 while gen<MAXGEN
     FitnV=ranking(ObjV);                                 % 分配适应度值(Assign fitness values)
@@ -64,16 +68,17 @@ while gen<MAXGEN
     gen=gen+1;                                           % 代计数器增加
     
     % 输出最优解及其序号，并在目标函数图像中标出，Y为最优解,I为种群的序号
-    trace(1, gen)=min(ObjV);                             % 遗传算法性能跟踪
+    % 遗传算法性能跟踪
+    trace(1, gen)=min(ObjV);
     trace(2, gen)=mean(ObjV);
     
     % 初始化
     if gen==1
         Val1=PVal;
         Val2=P;
-        MinVal=min(ObjV);                  % 最小时间
+        MinVal=min(ObjV);                  % 初始化最小调度工序时间
     end
-    % 记录最小的工序
+    % 记录最佳调度工序时间、最佳输出的调度工序、最小的调度工序时间
     if MinVal> trace(1,gen)
         Val1=PVal;
         Val2=P;
@@ -85,17 +90,19 @@ PVal=Val1;                  % 工序时间
 P=Val2;                     % 工序
 
 % 计算解的变化
+figure(1);
 hold on;
 plot([0,0],[0,0]);
-plot(trace(1,:));
+BestV=plot(trace(1,:),'r');
 hold on;
-plot(trace(2,:),'-.');grid;
-legend('解的变化','种群均值的变化');
+MeanV=plot(trace(2,:),'b');
+grid;
+legend([BestV,MeanV],'解的变化','种群均值的变化');
 % 显示结果
 figure(2);
 for i=1:WNumber
     val= P(1,i);
-    a=(mod(val,10))+1;
+    a=(mod(val,10))+1;                             % Modulus after division
     b=((val-a+1)/10);
     mText=Jm(b,a);
     PlotRec(PVal(1,i),PVal(2,i),mText);
